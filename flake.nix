8210cd2c3df7ae370d98b2fc1ad5c916b3a93519
glassesneo <glassesneo@protonmail.com>
2025-03-05 18:32:39 +0900

    build: enhance flake.nix

--- flake.nix
+++ flake.nix
@@ -11,7 +11,22 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        nimbleFile = builtins.readFile ./seiryu.nimble;
+        versionMatch = builtins.match ".*version *= *\"([0-9]+\\.[0-9]+\\.[0-9]+)\".*" nimbleFile;
+        version = builtins.elemAt versionMatch 0;
       in {
+        packages = {
+          seiryu = pkgs.stdenvNoCC.mkDerivation {
+            pname = "seiryu";
+            inherit version;
+            src = ./src;
+            buildInputs = [pkgs.nim];
+            installPhase = ''
+              mkdir -p $out/nim
+              cp -r $src $out/nim/seiryu
+            '';
+          };
+        };
         devShells = {
           default = pkgs.mkShell {
             packages = with pkgs; [
