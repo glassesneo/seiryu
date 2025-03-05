916ea141b9ec9ffa9d0547c4c8cdcec626b67864
glassesneo <glassesneo@protonmail.com>
2025-03-05 17:36:22 +0900

    build: introduce nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nim
+              nimble
+              nimlangserver
+              nph
+            ];
+          };
+        };
+      }
+    );
+}
